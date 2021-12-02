d8b1873e61d2e0688bafc5e05d8b19ea02e07ee6
Nata2401 <kuzyan.rd@yandex.ru>
2021-12-02 19:48:36 +0300

    Выполнено задание к уроку 6

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 /*jshint esversion: 9 */
 'use strict';
+const getRandomInt = function(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+};
+
+const isNum = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+const start = function() {
+    let rNumber = getRandomInt(100);
+    const game = function() {
+        const num = prompt('"Угадай число от 1 до 100"');
+        if (num === null) {
+            alert('Игра окончена');
+            return;
+        }
+        if (isNum(num)) {
+            const realNum = +num;
+            if (realNum > rNumber) {
+                alert('Загаданное число меньше');
+                game();
+            } else if (realNum < rNumber) {
+                alert('Загаданное число больше');
+                game();
+            } else {
+                if (confirm('Поздравляю, Вы угадали!!!')) {
+                    start();
+                } else {
+                    alert('Пока');
+                    return;
+                }
+            }
+        } else {
+            alert('Введите число!');
+            game();
+        }
+    };
+    game();
+};
+
+start();
